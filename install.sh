02aae39e73b02b1052caf08cb0732bf2171a1799
Brian <mgbrian@users.noreply.github.com>
2025-08-12 09:45:49 +0100

    Add Django ORM boilerplate.

--- install.sh
+++ install.sh
@@ -9,3 +9,23 @@ python3 -m venv $VENV_DIR || { echo "Error creating virtualenv."; exit 1; }
 source $VENV_DIR/bin/activate || { echo "Error activating virtualenv."; exit 1; }
 
 pip install -r requirements.txt
+
+if [ -f sample_env.py ] && [ ! -f env.py ]; then
+    echo ""
+    echo "Creating env.py from main repository's sample_env.py..."
+    cp sample_env.py env.py || { echo "Failed to create env.py."; exit 1; }
+fi
+
+if [ -f env.py ]; then
+    echo ""
+    echo "==========="
+    echo "   TODO:"
+    echo "==========="
+    echo "- Please update the variables in env.py accordingly."
+
+    # Reminder to install and migrate Django database. The modified manage.py
+    # here ensures the dabatase is created if it doesn't exist...
+    if [ -f manage.py ]; then
+        echo "- Once you've added the DB env variables, run python3 manage.py migrate..."
+    fi
+fi
---
+++ sample_env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("DJANGO_SECRET_KEY", "<some long string>")
+
+os.environ.setdefault("POSTGRES_DB_USER", "")
+os.environ.setdefault("POSTGRES_DB_PASSWORD", "")
+os.environ.setdefault("POSTGRES_HOST", "")
+os.environ.setdefault("POSTGRES_PORT", "")
