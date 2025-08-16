b45d325761eb06d7cf8c2843e04f702dedaf54ea
Brian <mgbrian@users.noreply.github.com>
2025-08-16 11:59:39 +0100

    Revert KeyboardInterrupt handler as that's already caught by signal handler.

--- main.py
+++ main.py
@@ -271,16 +271,6 @@ def main(argv=None):
 
         return 1
 
-    except KeyboardInterrupt:
-        logging.info("KeyboardInterrupt.")
-        try:
-            app.shutdown()
-            return 0
-
-        except Exception as e:
-            logging.exception(f"Error encountered while shutting down: {e}")
-            return 1
-
 
 if __name__ == "__main__":
     sys.exit(main())
